2cf902f603d35878aae2917a81fe737e4148d267
DanielKwon0721 <sunki001@gmail.com>
2020-10-12 11:38:38 +0900

    Update sblib.h

--- sblib.h
+++ sblib.h
@@ -189,3 +189,241 @@ public:
 	//}
 
 //}  // namespace sblib
+
+/* 참고 class */
+#include <iostream>
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <functional>
+#include <vector>
+#include <queue>
+#include <condition_variable>
+#include <chrono>
+
+typedef enum __ENUM_COND_VAR_RSLT__
+{
+	COND_VAR_RSLT_TIMEOUT = 0,
+	COND_VAR_RSLT_SIGNALED
+} ENUM_COND_VAR_RSLT ;
+
+///////////////////////////////////////////////////////////////////////////////
+class CondVar
+{
+	public:
+		CondVar()  = default;
+		~CondVar() = default;
+
+		void NotifyOne()
+		{
+			std::unique_lock<std::mutex> lock (cond_var_lock_);
+			is_notified_ = true;
+			cond_var_.notify_one();
+		}
+
+		void NotifyAll()
+		{
+			std::unique_lock<std::mutex> lock (cond_var_lock_);
+			is_notified_ = true;
+			cond_var_.notify_all();
+		}
+
+		void WaitForSignal()
+		{
+			std::unique_lock<std::mutex> lock (cond_var_lock_);
+			while (!is_notified_) 
+			{
+				cond_var_.wait(lock );
+			}    
+			if(!is_all_waiting_end_)
+			{
+				is_notified_=false;
+			}
+		}
+
+		ENUM_COND_VAR_RSLT WaitForSignalTimeout(int timeout_secs)
+		{
+			std::unique_lock<std::mutex> lock (cond_var_lock_);
+			std::cv_status ret = std::cv_status::no_timeout;
+
+			auto duration_sec = std::chrono::seconds(timeout_secs);
+
+			while(!is_notified_ && std::cv_status::timeout !=ret)
+			{ 
+				ret=cond_var_.wait_for(lock, duration_sec);
+			}
+
+			if(!is_all_waiting_end_)
+			{
+				is_notified_=false;
+			}
+
+			if(std::cv_status::timeout ==ret)
+			{
+				return COND_VAR_RSLT_TIMEOUT;
+			}
+			return COND_VAR_RSLT_SIGNALED;
+		}
+
+		void SetAllWaitingEnd()
+		{
+			is_all_waiting_end_ = true;
+		}
+
+	private:    
+		std::mutex              cond_var_lock_ ;
+		std::condition_variable cond_var_ ;
+		bool is_notified_ {false};
+		bool is_all_waiting_end_ {false};
+
+};
+
+///////////////////////////////////////////////////////////////////////////////
+class KoThreadPool 
+{
+	public:
+		KoThreadPool()  = default;
+
+		~KoThreadPool()
+		{
+			if(!stop_flag_)
+			{
+				Terminate();
+			}
+		}
+
+		bool InitThreadPool(int num_of_threads= 0) 
+		{
+			if(num_of_threads == 0 )
+			{
+				int concurrency_hint = std::thread::hardware_concurrency();
+				std::cout <<"std::thread::hardware_concurrency() = " << concurrency_hint << "\n";
+
+				if(concurrency_hint > 1 )
+				{
+					num_of_threads_  = concurrency_hint ;
+				}
+				else
+				{
+					std::cerr << "std::thread::hardware_concurrency() failed! specify number of thread \n";
+					return false;
+				}
+			}
+			else
+			{
+				num_of_threads_  = num_of_threads ;
+			}
+
+
+			for(int i=0; i < num_of_threads_ ; i++)
+			{
+				//vec_thread_.push_back( std::thread (&KoThreadPool::WorkerThreadRoutine, this, i) ) ;
+				vec_thread_.emplace_back(std::thread(&KoThreadPool::WorkerThreadRoutine, this, i));
+			}
+
+			return true;
+		}
+
+		void AssignTask( std::function<void()> & func ) 
+		{
+			PushQueue(func);
+			cond_var_.NotifyOne();
+		}
+
+		void Terminate(bool terminate_immediately=false)
+		{
+			stop_flag_ = true;
+			is_terminate_immediately_ = terminate_immediately;
+
+			if(is_terminate_immediately_)
+			{
+				cond_var_.SetAllWaitingEnd(); //terminate immediately
+			}
+
+			cond_var_.NotifyAll();
+
+			for(size_t i = 0; i < vec_thread_.size(); i++)
+			{
+				if (vec_thread_[i].joinable())
+				{
+					vec_thread_[i].join();
+				}
+			}
+		}
+
+		bool IsQueueEmpty()
+		{
+			std::unique_lock<std::mutex> lock(mutex_);
+			return task_queue_.empty();
+		}
+
+	private:
+
+		std::queue<std::function<void()>  > task_queue_ ;
+		std::mutex    mutex_      ;
+		std::atomic<bool>   stop_flag_ {false};
+		std::atomic<bool>   is_terminate_immediately_ {false};
+		std::vector<std::thread> vec_thread_ ;
+		CondVar     cond_var_ ;
+		int         num_of_threads_ {-1};
+
+	private:
+
+		void PushQueue( std::function<void()> const & value) 
+		{
+			std::unique_lock<std::mutex> lock(mutex_);
+			task_queue_.push(value);
+		}
+
+		bool  PopQueue() 
+		{
+			std::function<void()> func ;
+			{
+				std::unique_lock<std::mutex> lock(mutex_);
+				if (task_queue_.empty())
+				{
+					return false;
+				}
+
+				func = task_queue_.front();
+				task_queue_.pop();
+			}
+
+			func();
+
+			return true;
+		}
+
+
+		void WorkerThreadRoutine(int index)
+		{
+			while (true) 
+			{
+				if(IsQueueEmpty())
+				{
+					if(stop_flag_)
+					{
+						//graceful terminate
+						cond_var_.NotifyAll();
+						return; 
+					}
+					cond_var_.WaitForSignal();
+				}
+
+				if(is_terminate_immediately_ )
+				{
+					//force terminate
+					std::cout <<"WorkerThreadRoutine terminated: index " << index << "\n";
+					return;
+				}
+
+				PopQueue() ;
+			} 
+		}
+		
+		KoThreadPool(const KoThreadPool &) = delete;
+		KoThreadPool(KoThreadPool &&) = delete;
+		KoThreadPool & operator=(const KoThreadPool &) = delete;
+		KoThreadPool & operator=(KoThreadPool &&) = delete;
+};
+
